016e13205e160b9fbafa2d0e025cde8cd6928a9c
agomez@contifico.com <info@bit3000.com>
2017-02-03 01:29:42 -0500

    repositorio inicial

---
+++ app/src/main/java/com/bit3000/servicetest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bit3000.servicetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
